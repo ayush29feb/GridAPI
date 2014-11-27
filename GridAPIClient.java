13625608fd3d1e098b0a02de7f9154dfb26f1eb2
Ayush <ayush29f@uw.edu>
2014-11-27 06:12:15 -0800

    Created a subclass of Grid to overide paint method

--- GridAPIClient.java
+++ GridAPIClient.java
@@ -1,8 +1,34 @@
-import java.awt.Graphics;
+import java.awt.*;
+
 public class GridAPIClient {
 	
 	public static void main(String[] args) {
-		Grid graph = new Grid(800, 600);
+		YourGrid graph = new YourGrid(600, 600);
 		
+	}
+}
+
+class YourGrid extends Grid {
+	
+	@Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+
+	public YourGrid(int width, int height, int originX, int originY, int boxSize, int boxUnit) {
+		super(width, height, originX, originY, boxSize, boxUnit);
+	}
+
+	public YourGrid() {
+		super(Toolkit.getDefaultToolkit().getScreenSize().width, 
+			 Toolkit.getDefaultToolkit().getScreenSize().height);
+	}
+
+	public YourGrid(int width, int height) {
+		super(width, height, width/2, height/2);
+	}
+
+	public YourGrid(int width, int height, int originX, int originY) {
+		super(width, height, originX, originY, 50, 5);
 	}
 }
